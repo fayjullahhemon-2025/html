da570d5861ad120e4dbd1a7d3064434828fb57d3
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-07-14 22:34:00 +0600

    itro-to-js folder uploaded -> variable.js updated

---
+++ intro-to-js/variable.js
@@ -0,0 +1,18 @@
+//Variable
+var weight = 38;
+var temperature = 38;
+var people = 23;
+
+console.log(temperature);
+
+//let , const
+
+let price= 100;
+price = 50;
+price=300;
+
+console.log(price);
+
+const a = 34;
+// a = 32; //error
+console.log(a);
